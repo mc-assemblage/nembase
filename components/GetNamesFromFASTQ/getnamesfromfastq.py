aaeb9e19411084781a4acefcd91b9c4f13e37ca0
Michael Clarke <mclarke2@podge.genepool.private>
2013-01-30 04:34:27 +0000

    Bugfixes

--- components/GetNamesFromFASTQ/getnamesfromfastq.py
+++ components/GetNamesFromFASTQ/getnamesfromfastq.py
@@ -6,7 +6,7 @@ outfh = open(namelist, 'w')
 fqp = FastqParser()
 
 for rec in fqp.parse(open(fastqfile)):
-	outfh.write("%s\n" % rec.header[1:])
+	outfh.write("%s\n" % rec.header)
 outfh.close()
 
 
