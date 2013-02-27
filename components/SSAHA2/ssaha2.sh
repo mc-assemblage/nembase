5b2ccb831326bbe8bb4cd6653eeb19c7badb848c
Michael Clarke <mclarke2@podge.genepool.private>
2013-02-27 15:39:21 +0000

    Merged the SSAHA2 and UniVecScreen components.

--- components/SSAHA2/ssaha2.sh
+++ components/SSAHA2/ssaha2.sh
@@ -3,19 +3,24 @@
 
 
 vectorfile=$( getinput "vector")
-fastqfile=$( getinput "fastqfile")
+inputfile=$( getinput "inputfile")
 ssaha2out=$( getoutput "ssaha2out")
 mode=$( getparameter "rtype")
 
 
-echo $fastqfile >> $logfile
+echo $inputfile >> $logfile
 echo $vectorfile >> $logfile
 
 if [ -n "$vectorfile" ] && [ -s "$vectorfile" ]
 then
-	ssaha2 -output ssaha2 -rtype $mode $fastqfile $vectorfile > $ssaha2out
-else
+	ssaha2 -output ssaha2 -rtype $mode $inputfile $vectorfile > $ssaha2out
+elif [ -n "$vectorfile" ]
+then
+	# custom vector file is empty, just create an empty output file
 	touch $ssaha2out
+else
+	# use the default vector file (downloaded from NCBI UniVec database)
+	ssaha2 -output ssaha2 -rtype $mode $inputfile UniVec > $ssaha2out
 fi
 
 
